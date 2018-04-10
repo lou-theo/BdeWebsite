bdc2831eddc1ce58d9e95f6357dd6a74d0ed53bc
ValentineTilly <valentine.tilly@viacesi.fr>
2018-04-10 14:56:45 +0200

    For message

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -17,6 +17,26 @@ class WelcomeController extends Controller
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'WelcomeController',
         ]);
+
+
+
+
+        /*public function contact(Request $request)
+    {
+    $defaultData = array('message' => 'Type your message here');
+    $form = $this->createFormBuilder($defaultData)
+    ->add('name', TextType::class)
+    ->add('email', EmailType::class)
+    ->add('message', TextareaType::class)
+    ->add('send', SubmitType::class)
+    ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+    // data is an array with "name", "email", and "message" keys
+    $data = $form->getData();
+    }*/
     }
 }
 
---
+++ src/Form/Contact.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class Form
+{
+
+
+// ... render the form
+}
