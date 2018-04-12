be94ad5ac799413455069cafbec8f2996baea41b
lou-theo <loutheo.laurent@viacesi.fr>
2018-04-12 16:14:24 +0200

    creation des pages events

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -8,9 +8,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class EventController extends Controller
 {
     /**
-     * @Route("/event", name="event")
+     * @Route("/evenements", name="future_event")
      */
     public function indexAction()
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+
+    /**
+     * @Route("/evenements/boite-a-idee", name="event_idea")
+     */
+    public function ideaEventAction()
+    {
+
+
+        return $this->render('event/idea_event.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+
+    /**
+     * @Route("/evenements/passes", name="past_event")
+     */
+    public function pastEventAction()
     {
         return $this->render('event/index.html.twig', [
             'controller_name' => 'EventController',
