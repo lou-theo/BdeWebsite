88710c557d5e27fa7a290cf10c31b7dcbc723a55
ValentineTilly <valentine.tilly@viacesi.fr>
2018-04-12 10:59:57 +0200

    SwiftMailer qui marche

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response; 
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
