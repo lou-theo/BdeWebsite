c1e7022a97f4e514b7c921c0123435469493df7d
lou-theo <loutheo.laurent@viacesi.fr>
2018-04-18 19:26:59 +0200

    fonctions ajax pour ajouter/retirer un like, vote et participation

--- src/Controller/Events/FutureEventController.php
+++ src/Controller/Events/FutureEventController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Events;
 
 use App\Entity\Event;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,7 +34,7 @@ class FutureEventController extends Controller
      *
      * @Route("/evenements/a-venir/{idEvent}", name="future_event_details", requirements={"idEvent" = "\d+"})
      */
-    public function pastEventDetailsAction(int $idEvent)
+    public function futureEventDetailsAction(int $idEvent)
     {
         $futureEvent = $this->getDoctrine()
             ->getRepository(Event::class)
@@ -53,6 +54,80 @@ class FutureEventController extends Controller
             'futureEvent' => $futureEvent,
             'user' => $user,
         ]);
+    }
+
+    /**
+     * @param int $idEvent
+     * @return JsonResponse
+     * @throws \LogicException
+     *
+     * @Route("/ajax/a-venir/participer/{idEvent}", name="event_add_participate", methods="GET", requirements={"idEvent" = "\d+"})
+     */
+    public function ajaxEventAddParticipate(int $idEvent): JsonResponse
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $event = $this->getDoctrine()
+            ->getRepository(Event::class)
+            ->findOneBy(['id' => $idEvent]);
+
+        if (!$event) {
+            return $this->json(['status' => 'error', 'message' => 'Aucun evenement associe à l url']);
+        }
+
+        $user = $this->getUser();
+
+        if ($event->getUsersParticipate()->contains($user) ) {
+            return $this->json(['status' => 'error', 'message' => 'Vous participez deja a cet evenement']);
+        }
+
+        if ($event->getEventDate() < new \DateTime("now")) {
+            return $this->json(['status' => 'error', 'message' => 'Vous ne pouvez pas participer a un evenement passe']);
+        }
+
+        $event->addUserParticipate($user);
+        $em->flush();
+
+        return $this->json(['status' => 'success', 'message' => 'La participation a bien ete pris en compte']);
+    }
+
+    /**
+     * @param int $idEvent
+     * @return JsonResponse
+     * @throws \LogicException
+     *
+     * @Route("/ajax/a-venir/de-participer/{idEvent}", name="event_remove_participate", methods="GET", requirements={"idEvent" = "\d+"})
+     */
+    public function ajaxEventRemoveParticipate(int $idEvent): JsonResponse
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $event = $this->getDoctrine()
+            ->getRepository(Event::class)
+            ->findOneBy(['id' => $idEvent]);
+
+        if (!$event) {
+            return $this->json(['status' => 'error', 'message' => 'Aucun evenement associe à l url']);
+        }
+
+        $user = $this->getUser();
+
+        if (!$event->getUsersParticipate()->contains($user) ) {
+            return $this->json(['status' => 'error', 'message' => 'Vous ne participez a cet evenement']);
+        }
+
+        if ($event->getEventDate() < new \DateTime("now")) {
+            return $this->json(['status' => 'error', 'message' => 'Vous ne pouvez pas vous desinscrire d un evenement passe']);
+        }
+
+        $event->removeUserParticipate($user);
+        $em->flush();
+
+        return $this->json(['status' => 'success', 'message' => 'Le retrait de participation a bien ete pris en compte']);
     }
 
     /**
